27d0b14ee1727a09d1f5724579ba5f2281fbad7c
Maxence Retier <m.retier@student.isartdigital.com>
2022-09-07 17:33:23 +0200

    [Meteor] is moving correctly + delete half of the population

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -8,41 +8,52 @@ public class Meteor : MonoBehaviour
 
     private GameObject toFollow = null;
     private GameObject instMeteor = null;
+
     public GameObject meteor = null;
 
-    bool isShoot = false;
+    Population population = null;
 
     private float elapsedTime = 0f;
     public float speed = 2f;
+    public float damage = 0.5f;
+
+    private bool isShooting = false;
 
     // Start is called before the first frame update
     void Start()
     {
         select = GetComponent<Selector>();
+        population = GetComponent<Population>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (select.hit.collider != null && Input.GetMouseButtonDown(1))
+        if (instMeteor == null && isShooting)
+        {
+            Debug.Log(toFollow.GetComponent<Population>().populationRate);
+            toFollow.GetComponent<Population>().populationRate *= 1 - damage;
+            Debug.Log(toFollow.GetComponent<Population>().populationRate);
+
+            isShooting = false;
+            elapsedTime = 0;
+        }
+
+        if (select.hit.collider != null && Input.GetKeyDown(KeyCode.Alpha1) && !isShooting)
         {
             toFollow = select.hit.transform.gameObject;
             instMeteor = Instantiate(meteor, Camera.main.transform.position, Quaternion.identity);
-            isShoot = true;
+            Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH");
+
+            isShooting = true;
         }
         
-        if (instMeteor != null && isShoot)
+        if (instMeteor != null && isShooting)
         {
             elapsedTime += Time.deltaTime;
             float percentageComplete = elapsedTime / speed;
 
-            instMeteor.transform.position = Vector3.Lerp(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z), toFollow.transform.position, percentageComplete);
+            instMeteor.transform.position = Vector3.Lerp(instMeteor.transform.position, toFollow.transform.position, percentageComplete);
         }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Destroy");
-        Destroy(this.gameObject);
     }
 }
